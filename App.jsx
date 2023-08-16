4a8aa355036aa3d22bd57cf34a4d759117a0045d
Johannaussico1998 <106630475+Johannaussico1998@users.noreply.github.com>
2023-08-16 12:46:36 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import NavBar from './Components/NavBar'
+import Footer from './Components/Footer'
+import Content from './Components/Content'
+import HomeGrid from './Components/HomeGrid'
+import TopBar from './Components/TopBar'
+function App() {
+  return (
+    <>
+      <TopBar/>
+      <NavBar/>
+      <Content/>
+      <HomeGrid/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
