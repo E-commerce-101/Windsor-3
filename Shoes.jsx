ece605e3c13ccc3c7ae3127150a438ffcc776695
Matlhogonolo05 <hlogymoswang@gmail.com>
2023-08-23 12:37:09 +0200

    Add files via upload

---
+++ Shoes.jsx
@@ -0,0 +1,174 @@
+import Picture_1 from '../Shoes/picture1.jpg'
+import Picture_2 from '../Shoes/picture2.jpg'
+import Picture_3 from '../Shoes/picture3.jpg'
+import Picture_4 from '../Shoes/picture4.jpg'
+import Picture_5 from '../Shoes/picture5.jpg'
+import Picture_6 from '../Shoes/picture6.jpg'
+import Picture_7 from '../Shoes/picture7.jpg'
+import Picture_8 from '../Shoes/picture8.jpg'
+import Picture_9 from '../Shoes/picture9.jpg'
+const Content =()=>{
+    return(
+        <section className="py-16">
+            <div className="container">
+                <div className="mb-5">
+                    <h2 className="text -3xl Font-bold">Shoes</h2>
+                </div>
+                <div className="grid grid-cols-9 gap-8">
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_1} alt="" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">Shoe1</h3>
+                                <h4 className="font-bold text-3xl mt-2">350</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_2} alt="" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">Shoe1</h3>
+                                <h4 className="font-bold text-3xl mt-2">350</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_3} alt="" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">Shoe1</h3>
+                                <h4 className="font-bold text-3xl mt-2">350</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_4} alt="" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">Shoe1</h3>
+                                <h4 className="font-bold text-3xl mt-2">350</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_5} alt="" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">Shoe1</h3>
+                                <h4 className="font-bold text-3xl mt-2">350</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_6} alt="" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">Shoe1</h3>
+                                <h4 className="font-bold text-3xl mt-2">350</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_7} alt="" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">Shoe1</h3>
+                                <h4 className="font-bold text-3xl mt-2">350</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_8} alt="" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">Shoe1</h3>
+                                <h4 className="font-bold text-3xl mt-2">350</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_9} alt="" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">Shoe1</h3>
+                                <h4 className="font-bold text-3xl mt-2">350</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+
+                    
+
+                </div>
+                
+
+            </div>
+
+        </section>
+
+    )
+}
+export default Shoes
