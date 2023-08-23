ba9d4286ae248c3e9362f1bc95246c47ad7d3ee3
Matlhogonolo05 <hlogymoswang@gmail.com>
2023-08-23 17:05:02 +0200

    Add files via upload

---
+++ Accessories.jsx
@@ -0,0 +1,174 @@
+import Picture_1 from '../AccessoriesPict/Accs1.jpg'
+import Picture_2 from '../AccessoriesPict/Accs2.jpg'
+import Picture_3 from '../AccessoriesPict/Accs3.jpg'
+import Picture_4 from '../AccessoriesPict/Accs4.jpg'
+import Picture_5 from '../AccessoriesPict/Accs5.jpg'
+import Picture_6 from '../AccessoriesPict/Accs6.jpg'
+import Picture_7 from '../AccessoriesPict/Accs7.jpg'
+import Picture_8 from '../AccessoriesPict/Accs8.jpg'
+import Picture_9 from '../AccessoriesPict/Accs9.jpg'
+const Accessories =()=>{
+    return(
+        <section className="py-16">
+            <div className="container">
+                <div className="mb-5">
+                    <h2 className="text -3xl Font-bold">Shoes</h2>
+                </div>
+                <div className="grid grid-cols-9 gap-8">
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_1} alt="https://www.windsorstore.com/collections/accessories-jewelry-necklaces-chokers/products/all-in-the-shine-necklace-set-070020095903#from_smart_nav|0" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">All in the Shine Necklace</h3>
+                                <h4 className="font-bold text-3xl mt-2">R280.92</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_2} alt="https://www.windsorstore.com/collections/accessories-jewelry-necklaces-chokers/products/perfect-and-posh-pearl-choker-necklace-070021490072#from_smart_nav|1" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">Pearl Chocker Necklace</h3>
+                                <h4 className="font-bold text-3xl mt-2">R158.12</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_3} alt="https://www.windsorstore.com/collections/accessories-jewelry-necklaces-chokers/products/pretty-luxe-pearl-flower-choker-necklace-070021494072#from_smart_nav|2" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">Flower Chocker Necklace</h3>
+                                <h4 className="font-bold text-3xl mt-2">R176,19</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_4} alt="https://www.windsorstore.com/collections/accessories-jewelry-necklaces-chokers/products/luxe-glam-rhinestone-choker-necklace-070021346006#from_smart_nav|6" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">Glam Rhinestone Necklace</h3>
+                                <h4 className="font-bold text-3xl mt-2">R315.76</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_5} alt="https://www.windsorstore.com/collections/accessories-purses-bags/products/glam-affair-rhinestone-trim-mini-handbag-072040703001#from_smart_nav|0" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">Trim Mini Handbag</h3>
+                                <h4 className="font-bold text-3xl mt-2">R416.24</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_6} alt="https://www.windsorstore.com/collections/accessories-purses-bags/products/wing-span-rhinestone-butterfly-crossbody-bag-072040720010#from_smart_nav|4" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">Wing Rhinestone Butterfly Bag</h3>
+                                <h4 className="font-bold text-3xl mt-2">R706.60</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_7} alt="https://www.windsorstore.com/collections/accessories-purses-bags/products/pretty-and-glam-jelly-satchel-bag-072031293060#from_smart_nav|14" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">Jelly Satchel Bag</h3>
+                                <h4 className="font-bold text-3xl mt-2">R297.44</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_8} alt="https://www.windsorstore.com/collections/accessories-purses-bags/products/sleek-and-chic-mini-tote-072030557001#from_smart_nav|15" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">Minnie Tote</h3>
+                                <h4 className="font-bold text-3xl mt-2">R382.05</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_9} alt="https://www.windsorstore.com/collections/accessories-sunglasses/products/summer-is-here-oversized-round-sunglasses-070081013020#from_smart_nav|1" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl">Oversized Round Sunglasse </h3>
+                                <h4 className="font-bold text-3xl mt-2">R152.12</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+
+
+
+                </div>
+
+
+            </div>
+
+        </section>
+
+    )
+}
+export default Accessories
