afe34bde037fc5618ad47ea82e6a8f9504b3861e
Matlhogonolo05 <hlogymoswang@gmail.com>
2023-08-24 19:57:15 +0200

    Add files via upload

---
+++ Heels.jsx
@@ -0,0 +1,228 @@
+import Picture_1 from '../HeelsPict/heel1.jpg'
+import Picture_2 from '../HeelsPict/heel2.jpg'
+import Picture_3 from '../HeelsPict/heel3.jpg'
+import Picture_4 from '../HeelsPict/heel4.jpg'
+import Picture_5 from '../HeelsPict/heel5.jpg'
+import Picture_6 from '../HeelsPict/heel6.jpg'
+import Picture_7 from '../HeelsPict/heel7.jpg'
+import Picture_8 from '../HeelsPict/heel8.jpg'
+import Picture_9 from '../HeelsPict/heel9.jpg'
+import Picture_10 from '../HeelsPict/heel10.jpg'
+import Picture_11 from '../HeelsPict/heel11.jpg'
+import Picture_12 from '../HeelsPict/heel12.jpg'
+
+
+
+const Heels =()=>{
+    return(
+        <section className="py-16">
+            <div className="container">
+                <div className="mb-5">
+                    <h2 className="text-3xl Font-bold">Heels</h2>
+                </div>
+                <div className="grid grid-cols-3 gap-8">
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_1} alt="https://www.windsorstore.com/collections/shoes-heels-high/products/classic-moves-faux-leather-stiletto-heels-074014418072#from_smart_nav|3" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl mt-0">Faux Leather Stilleto Heels</h3>
+                                <h4 className="font-bold text-3xl mt-2">595.96 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={Picture_2} alt="https://www.windsorstore.com/collections/shoes-heels-high/products/perfectly-on-point-pearl-clear-stiletto-heels-074014281092#from_smart_nav|11" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Pearl Clear Stilletto Heels</h3>
+                                <h4 className="font-bold text-3xl mt-2">758.12 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={Picture_3} alt="https://www.windsorstore.com/collections/shoes-heels-high/products/chic-glam-rhinestone-lace-up-stiletto-heels-074014171001#from_smart_nav|15" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Rhinestone Lace Up Heels</h3>
+                                <h4 className="font-bold text-3xl mt-2">436.19 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_4} alt="https://www.windsorstore.com/collections/shoes-heels-high/products/trendy-icon-butterfly-accent-stiletto-heels-074014453001#from_smart_nav|17" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Butterfly Accent Stiletto Heels</h3>
+                                <h4 className="font-bold text-3xl mt-2">795.28  ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={Picture_5} alt="https://www.windsorstore.com/collections/shoes-heels-high/products/raise-the-bar-strappy-block-heel-mules-074014403200#from_smart_nav|23" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Strappy Block Heel Mules</h3>
+                                <h4 className="font-bold text-3xl mt-2">516.44 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={Picture_6} alt="https://www.windsorstore.com/collections/shoes-heels-high/products/all-that-glitter-block-heels-074010320700#from_smart_nav|30" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Glitter Block Heels</h3>
+                                <h4 className="font-bold text-3xl mt-2">506.60 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_7} alt="https://www.windsorstore.com/collections/shoes-heels-high/products/chic-update-strappy-block-heels-074014526010#from_smart_nav|33" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Strappy Block Heels</h3>
+                                <h4 className="font-bold text-3xl mt-2">597.04 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_8} alt="https://www.windsorstore.com/collections/shoes-heels-high/products/draw-attention-platform-stiletto-heels-074014313001#from_smart_nav|43" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Platform Stiletto Heels</h3>
+                                <h4 className="font-bold text-3xl mt-2">482.75 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_9} alt="https://www.windsorstore.com/collections/shoes-heels-high/products/spark-of-glamour-rhinestone-spiral-stiletto-heels-074014352001#from_smart_nav|40" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Rhinestone Spiral Stiletto Heels </h3>
+                                <h4 className="font-bold text-3xl mt-2">693.52 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={Picture_10} alt="https://www.windsorstore.com/collections/shoes-heels-high/products/glam-illusion-rhinestone-stiletto-mules-074014409006#from_smart_nav|50" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Glam Illusion Rhinestone Stiletto Mules</h3>
+                                <h4 className="font-bold text-3xl mt-2">586.59 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={Picture_11} alt="https://www.windsorstore.com/collections/shoes-heels-high/products/winged-goddess-butterfly-rhinestone-mules-074014204010#from_smart_nav|52" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Butterfly Rhinestone Mules</h3>
+                                <h4 className="font-bold text-3xl mt-2">676.49 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={Picture_12} alt="https://www.windsorstore.com/collections/shoes-heels-high/products/sweet-dream-faux-pearl-block-heels-074014119010#from_smart_nav|54" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0"> Faux Pearl Block Heels</h3>
+                                <h4 className="font-bold text-3xl mt-2">679.69 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+
+
+
+                </div>
+
+
+            </div>
+
+        </section>
+
+    )
+}
+export default Heels
