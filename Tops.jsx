abe947dc54e48ce484e84b455a3fe59723220c7c
Matlhogonolo05 <hlogymoswang@gmail.com>
2023-08-24 21:05:29 +0200

    Add files via upload

---
+++ Tops.jsx
@@ -0,0 +1,226 @@
+import Picture_1 from '../TopsPict/top1.jpg'
+import Picture_2 from '../TopsPict/top2.jpg'
+import Picture_3 from '../TopsPict/top3.jpg'
+import Picture_4 from '../TopsPict/top4.jpg'
+import Picture_5 from '../TopsPict/top5.jpg'
+import Picture_6 from '../TopsPict/top6.jpg'
+import Picture_7 from '../TopsPict/top7.jpg'
+import Picture_8 from '../TopsPict/top8.jpg'
+import Picture_9 from '../TopsPict/top9.jpg'
+import Picture_10 from '../TopsPict/top10.jpg'
+import Picture_11 from '../TopsPict/top11.jpg'
+import Picture_12 from '../TopsPict/top12.jpg'
+
+const Tops = () =>{
+    return(
+        <section className="py-16">
+            <div className="container">
+                <div className="mb-5">
+                    <h2 className="text-3xl Font-bold">Tops</h2>
+                </div>
+                <div className="grid grid-cols-3 gap-8">
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_1} alt="https://www.windsorstore.com/collections/tops/products/glitzy-muse-rhinestone-fishnet-crop-top-060014645001#from_smart_nav|1" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl mt-0">Glitzy Muse Rhinestone Fishnet Top</h3>
+                                <h4 className="font-bold text-3xl mt-2">615.89 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={Picture_2} alt="https://www.windsorstore.com/collections/tops/products/after-hours-glow-metallic-strapless-bodysuit-060014896051#from_smart_nav|2" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Glow Metallic Strapless Bodysuit</h3>
+                                <h4 className="font-bold text-3xl mt-2">715.56 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={Picture_3} alt="https://www.windsorstore.com/collections/tops/products/mesh-around-square-neck-crop-top-060014743200#from_smart_nav|6" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Mesh Around Square Neck Crop Top</h3>
+                                <h4 className="font-bold text-3xl mt-2">456.69 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_4} alt="https://www.windsorstore.com/collections/tops/products/irresistible-lace-bodysuit-060019464001#from_smart_nav|12" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Irresistible Lace Bodysuit</h3>
+                                <h4 className="font-bold text-3xl mt-2">595.28  ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={Picture_5} alt="https://www.windsorstore.com/collections/shoes-heels-high/products/raise-the-bar-strappy-block-heel-mules-074014403200#from_smart_nav|23" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Ruched Mesh Bodysuits</h3>
+                                <h4 className="font-bold text-3xl mt-2">676.42 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={Picture_6} alt="https://www.windsorstore.com/collections/tops/products/total-babe-satin-mesh-corset-top-060014591001#from_smart_nav|30" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Satin Mesh Corset Top</h3>
+                                <h4 className="font-bold text-3xl mt-2">656.60 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_7} alt="https://www.windsorstore.com/collections/tops/products/naturally-a-classic-tweed-bustier-060032194001#from_smart_nav|42" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">A Classic Tweed Bustier</h3>
+                                <h4 className="font-bold text-3xl mt-2">478.04 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_8} alt="https://www.windsorstore.com/collections/tops/products/rock-n-roll-zip-front-cropped-graphic-tee-060021608001#from_smart_nav|14" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Rock 'N' Roll Zip-Front Cropped Graphic Tee</h3>
+                                <h4 className="font-bold text-3xl mt-2">362.75 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={Picture_9} alt="https://www.windsorstore.com/collections/tops/products/sheer-coverage-mesh-sleeve-cutout-crop-top-060014910001#from_smart_nav|43" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Sheer Coverage Mesh Sleeve Cutout Crop Top </h3>
+                                <h4 className="font-bold text-3xl mt-2">416.52 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={Picture_10} alt="https://www.windsorstore.com/collections/tops/products/fly-by-satin-butterfly-crop-top-060032136020#from_smart_nav|107" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Satin Butterfly Crop Top</h3>
+                                <h4 className="font-bold text-3xl mt-2">456.59 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={Picture_11} alt="https://www.windsorstore.com/collections/occasions-going-out-tops/products/reigning-lace-crop-top-060038300001#from_smart_nav|4" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Reigning Lace Crop Top</h3>
+                                <h4 className="font-bold text-3xl mt-2">537.39 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={Picture_12} alt="https://www.windsorstore.com/collections/occasions-going-out-tops/products/blown-away-plunge-tie-back-crop-top-060014484033#from_smart_nav|23" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0"> Blown Away Plunge Tie Back Crop Top</h3>
+                                <h4 className="font-bold text-3xl mt-2">489.69 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+
+
+
+                </div>
+
+
+            </div>
+
+        </section>
+
+    )
+}
+export default Tops
