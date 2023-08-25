a700065bc8ebf159261f0bb8c7d971418f00f594
Johannaussico1998 <106630475+Johannaussico1998@users.noreply.github.com>
2023-08-26 00:11:56 +0200

    Add files via upload

---
+++ Matching sets/matching.jsx
@@ -0,0 +1,189 @@
+import matching1 from '../matching/matching1.png'
+import matching2 from '../matching/matching2.png'
+import matching3 from '../matching/matching3.png'
+import matching4 from '../matching/matching4.png'
+import matching5 from '../matching/matching5.png'
+import matching6 from '../matching/matching6.png'
+import matching7 from '../matching/matching7.png'
+import matching8 from '../matching/matching8.png'
+import matching9 from '../matching/matching9.png'
+import matching10 from '../matching/matching10.png'
+
+const Tops = () =>{
+    return(
+        <section className="py-16">
+            <div className="container">
+                <div className="mb-5">
+                    <h2 className="text-3xl Font-bold">Tops</h2>
+                </div>
+                <div className="grid grid-cols-3 gap-8">
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={matching1} alt="https://www.windsorstore.com/collections/tops/products/glitzy-muse-rhinestone-fishnet-crop-top-060014645001#from_smart_nav|1" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl mt-0">Hot Summer Nights Rhinestone Crop Top</h3>
+                                <h4 className="font-bold text-3xl mt-2">510.15. ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={matching2} alt="https://www.windsorstore.com/collections/tops/products/after-hours-glow-metallic-strapless-bodysuit-060014896051#from_smart_nav|2" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Sleek Sophistication Satin Blazer</h3>
+                                <h4 className="font-bold text-3xl mt-2">215.99 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={matching3} alt="https://www.windsorstore.com/collections/tops/products/mesh-around-square-neck-crop-top-060014743200#from_smart_nav|6" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Mesh Around Square Neck Crop Top</h3>
+                                <h4 className="font-bold text-3xl mt-2">456.69 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={matching4} alt="https://www.windsorstore.com/collections/tops/products/irresistible-lace-bodysuit-060019464001#from_smart_nav|12" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Bold Moves Woven Blazer</h3>
+                                <h4 className="font-bold text-3xl mt-2">455.00  ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={matching5} alt="https://www.windsorstore.com/collections/shoes-heels-high/products/raise-the-bar-strappy-block-heel-mules-074014403200#from_smart_nav|23" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">So Elevated Side Slit Skorts</h3>
+                                <h4 className="font-bold text-3xl mt-2">756.00 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object-cover" src={matching6} alt="https://www.windsorstore.com/collections/tops/products/total-babe-satin-mesh-corset-top-060014591001#from_smart_nav|30" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Attention Please Cargo Cropped Jacket</h3>
+                                <h4 className="font-bold text-3xl mt-2">690.60 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={matching7} alt="https://www.windsorstore.com/collections/tops/products/naturally-a-classic-tweed-bustier-060032194001#from_smart_nav|42" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Smooth Silhouette Crop Tube Top</h3>
+                                <h4 className="font-bold text-3xl mt-2">308.00 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={matching8} alt="https://www.windsorstore.com/collections/tops/products/rock-n-roll-zip-front-cropped-graphic-tee-060021608001#from_smart_nav|14" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Business-Chic Envelope Skort</h3>
+                                <h4 className="font-bold text-3xl mt-2">450.75 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={matching9} alt="https://www.windsorstore.com/collections/tops/products/sheer-coverage-mesh-sleeve-cutout-crop-top-060014910001#from_smart_nav|43" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Flirt With Desire Halter Cutout Mini Dress </h3>
+                                <h4 className="font-bold text-3xl mt-2">430.50 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <figure className="aspect-square rounded-xl overflow-hidden">
+                            <img  className="w-full h-full object cover" src={matching10} alt="https://www.windsorstore.com/collections/tops/products/sheer-coverage-mesh-sleeve-cutout-crop-top-060014910001#from_smart_nav|43" />
+                        </figure>
+                        <div className="flex justify-between mt-4 items-centre">
+                            <div className="">
+                                <h3 className="font-medium text-xl  mt-0">Smooth Silhouette Long Sleeve Bolero </h3>
+                                <h4 className="font-bold text-3xl mt-2">200.50 ZAR</h4>
+
+                            </div>
+                            <button className="bg-current px-4 rounded-lg aspect-square w-fit h-full">
+                                <span className="material-symbols-outlined text-white">local_mall</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    </div>
+
+
+                </div>
+
+        </section>
+
+    )
+}
+export default Tops
---
+++ Matching sets/ourstory.jsx
@@ -0,0 +1,26 @@
+import matching10 from '../matching/matching10.png'
+
+const ourstory = () =>{
+    return(
+        <div className='story-content'>
+            <h2>OUR STORY</h2>
+            <a href='#' className= 'nav-logo' alt='company logo'></a>
+            <p>
+            Beauty is eternal.  It is captured in the way you feel and in the way you look.  It is timeless. 
+            Every generation needs to feel beautiful.  Some things don’t change. Some things should not change.
+            Helping women feel beautiful has been our mission since we first opened our doors in 2023.
+            The fashions have changed but the MJAY dream has not. 
+            Throughout it all MJAY has made beauty accessible to all women, not just the celebrities but every 
+            woman in general.
+            Their caring vision of shared beauty affects everything we do.
+            Women come to MJAY to find that perfect look that makes them feel beautiful and we have been there for them 
+            for all of life’s important moments - your first kiss at the dance, graduation, the big interview and the 
+            wedding party.  Lives have been changed, and the memories will live forever.
+            Windsor continues to bring that Oasis experience to you in over 200 stores and through our online shop. 
+            Some things should never change.
+            </p>
+
+        </div>
+    )
+}
+export default ourstory
